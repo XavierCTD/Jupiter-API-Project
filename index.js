979d68ded2de4c24b9578f7717ece597ffef395b
XavierCTD <macfamlove01@gmail.com>
2025-01-26 20:11:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 // Star Wars API
 
-const starWarsPlanets = "https://www.swapi.tech/api/planets/1/";
-
-fetch(starWarsPlanets)
-  .then((repsonse) => {
-    if(!response.ok) {
-      throw new error(`HTTP Error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((Planets) => {
-    console.log(Planets);
-
-    const planetName = document.getElementById('navigation');
-    const navigationByName = planetName.querySelector('div');
-
-    for(let i = 0; i < Planets.length; i++) {
-      const planetDirection = document.createElement('li');
-      planetDirection.innerText = Planets[i].name;
-      navigationByName.appendChild(planetDirection);
-    }
-  })
-
-  .catch((error) => {
-    console.error('There was a problem with the fetch sequence:', error);
-    const planetName = document.getElementById('projects');
-    planetName.innerText = 'ERROR: Cannot load API'
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  fetch("https://www.swapi.tech/api/planets/1/")
+    .then((repsonse) => {
+      if(!response.ok) {
+        throw new error(`HTTP Error! Status: ${response.status}`);
+      }
+     return response.json();
+    })
+    .then((Planets) => {
+      console.log('API response:', Planets);
+    })
+    .catch((error) => {
+      console.error('There was a problem with the fetch sequence:', error);
+    });
+});
