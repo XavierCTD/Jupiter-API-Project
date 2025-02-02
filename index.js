f5169127a1ccc2ce8e7de2cbb48a341286fbf02d
XavierCTD <macfamlove01@gmail.com>
2025-02-02 17:33:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 // Star Wars API
 
+document.addEventListener("DOMContentLoaded", () => {
+
+const contentDiv = document.getElementById("content");    
+const charactersButton = document.getElementById("charactersBtn");
+const starshipsButton = document.getElementById("starshipsBtn");
+
+charactersButton.addEventListener("click", () => fetchData("people"));
+starshipsButton.addEventListener("click", () => fetchData("starships"));
+
 async function fetchData(endpoint) {
     try {
-        contentDiv.innerHTML = "Loading...";
-        const response = await fetch(`https://swapi.dev/api/`);
-        if (!response.ok) throw new Error("API error");
-        
+        contentDiv.innerHTML = "<p>Loading...</p>";
+        const response = await fetch(`https://swapi.dev/api/${endpoint}/`);
+        if (!response.ok) {
+            throw new error("API error");
+        }
         const data = await response.json();
         displayData(data.results, endpoint);
     } catch (error) {
-        contentDiv.innerHTML = `<p>Error loading data: ${error.message}</p>`;
+        contentDiv.innerHTML = `<p>Error: ${error.message}</p>`;
     }
 }
+
+fetchData();
+
+function displayData(items, type) {
+   contentDiv.innerHTML = "";
+   items.slice(0, 5)forEach(item => {
+       const objects = document.createElement("div");
+       objects.innerHTML = type === "people"
+       ? `<h3>${item.name}</h3><p>Height: ${item.height} cm</p>`
+       : `<h3>${item.name}</h3><p>Model: ${item.model}</p>`
+       contentDiv.appendChild(objects);
+   });
+}
+    
+});
